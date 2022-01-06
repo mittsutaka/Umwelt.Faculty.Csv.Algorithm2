e6a1546f6b9fc7f4add008013700559a794cc783
mittsutaka <mitsutaka.murao@tryeting.jp>
2022-01-06 10:45:40 +0900

    リファクタリング

--- Umwelt.Faculty.Csv.Algorithm2/Faculty.cs
+++ Umwelt.Faculty.Csv.Algorithm2/Faculty.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Umwelt.Faculty.Csv.Algorithm2.Models;
 
@@ -61,22 +62,23 @@ namespace Umwelt.Faculty.Csv.Algorithm2
             }
 
             //集計
-            var groups = records.GroupBy(t => new { col1 = t.Keys[0], col2 = t.Keys[1] }).ToList();
+            var groups = records.GroupBy(t => t.Keys, StringArrayEqualityComparer.Default).ToList();
+            var fieldsLength = records.First().Fields.Length;
 
             var outputRecords = (from g in groups
-                                 orderby g.Key.col1, g.Key.col2
-                                 let ave1 = Math.Round(g.Average(r => decimal.Parse(r.Fields[3].ToString())), 2)
-                                 let ave2 = Math.Round(g.Average(r => decimal.Parse(r.Fields[4].ToString())), 2)
-                                 let std1 = calculateStd(g,ave1,3)
-                                 let std2 = calculateStd(g,ave2,4)
+                                 orderby g.Key[0], g.Key[1]
+                                 let ave1 = CalculateAve(g, 3)
+                                 let ave2 = CalculateAve(g, 4)
+                                 let std1 = CalculateStd(g, ave1, 3)
+                                 let std2 = CalculateStd(g, ave2, 4)
                                  select new
                                  {
-                                     col1 = g.Key.col1,
-                                     col2 = g.Key.col2,
-                                     countSum = g.Sum(r => decimal.Parse(r.Fields[3].ToString())),
+                                     col1 = g.Key[0],
+                                     col2 = g.Key[1],
+                                     countSum = CaclulateSum(g, 3),
                                      countAve = ave1,
                                      countStd = std1,
-                                     priceSum = g.Sum(r => decimal.Parse(r.Fields[4].ToString())),
+                                     priceSum = CaclulateSum(g, 4),
                                      priceAve = ave2,
                                      priceStd = std2,
                                  }).ToList();
@@ -92,11 +94,21 @@ namespace Umwelt.Faculty.Csv.Algorithm2
             }
         }
 
-        private double calculateStd(IEnumerable<Record> g,decimal ave,int index)
+        private double CalculateStd(IEnumerable<Record> g, decimal ave, int index)
         {
             var count = g.Count();
             var sum = g.Sum(r => Math.Abs(decimal.Parse(r.Fields[index].ToString()) - ave));
             return Math.Sqrt((double)sum / count);
+        }
+
+        private decimal CalculateAve(IGrouping<string[], Record> group, int index)
+        {
+            return Math.Round(group.Average(r => decimal.Parse(r.Fields[index].ToString())), 2);
+        }
+
+        private decimal CaclulateSum(IGrouping<string[], Record> group, int index)
+        {
+            return group.Sum(r => decimal.Parse(r.Fields[index].ToString()));
         }
     }
 }
